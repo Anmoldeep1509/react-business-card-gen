608fce390ead68ef13cd39d6b27e688454ccb38c
Anmoldeep1509 <anmoldeep1509@gmail.com>
2021-03-23 23:52:48 +0530

    Reorganized Code and Installed NODE SASS

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header>
+        <h1><a href="">Logo</a></h1>
+        <nav>
+          <ul>
+            <li><a href="">Link 1</a></li>
+            <li><a href="">Link 2</a></li>
+            <li><a href="">Link 3</a></li>
+            <li><a href="">Link 4</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+    )
+}
+
+export default App
